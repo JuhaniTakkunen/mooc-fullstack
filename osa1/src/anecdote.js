02b25f6c9a03dbf64c09829d61d8c5c93aa78d1b
juhani.takkunen <juhani.takkunen@visma.com>
2018-07-10 12:43:10 +0300

    Tehtävä 1 valmis, pohdi vielä, pitäisikö index.js -tiedostoa kehittää.

---
+++ osa1/src/anecdote.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+function getRndInteger(min, max) {  // https://www.w3schools.com/js/js_random.asp
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const Button = (props) => {
+    return (
+        <button onClick={props.event}>{props.label}</button>
+    )
+};
+
+
+const Anecdote = (props) => {
+    return (
+        <p>{props.anecdotes[props.state.selected]}</p>
+    )
+};
+
+const Statistics = (props) => {
+    let max_votes = Math.max(...props.pisteet);
+    let index = props.pisteet.indexOf(max_votes);
+
+    return (
+        <p>Best anecdote with {max_votes} votes: <br />
+            <i>{props.anecdotes[index]}</i>
+        </p>
+    )
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: 0,
+            pisteet: new Array(this.props.anecdotes.length).fill(0)
+
+        };
+
+    }
+
+    changeSelected = () => {
+        this.setState({
+            selected: getRndInteger(0, this.props.anecdotes.length)
+        })
+    };
+
+    addVote = () => {
+        const kopio = [...this.state.pisteet];
+        kopio[this.state.selected] += 1;
+
+        this.setState({
+            pisteet: kopio
+        })
+    };
+
+
+    render() {
+        return (
+            <div>
+                <Anecdote anecdotes={this.props.anecdotes} state={this.state}/>
+                <Button event={this.changeSelected} label='next anecdote'/>
+                <Button event={this.addVote} label='vote'/>
+                <Statistics pisteet={this.state.pisteet} anecdotes={this.props.anecdotes} />
+            </div>
+        )
+    }
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+ReactDOM.render(
+    <App anecdotes={anecdotes}/>,
+    document.getElementById('root')
+);
